53e7531cca868fa15043b519d0184b2c7ac3f46a
rhuwellsamano <rhuwell.samano@gmail.com>
2019-04-05 17:28:07 -0400

    aye

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,8 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/, 'http://10.39.109.8:3000']
+
   # Do not eager load code on boot.
   config.eager_load = false
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create, :index]
+      # root 'welcome#index'
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
+      # mount ActionCable.server => '/'
       mount ActionCable.server => '/cable'
     end
   end
